e96072a52853a1ef81bf6df15f530f8d32472339
HaDucDev <haductrung392000@gmail.com>
2022-12-07 04:57:24 +0700

    update-erole

--- WebShopSPringMVC/src/main/java/hdth/com/utils/enums/ERole.java
+++ WebShopSPringMVC/src/main/java/hdth/com/utils/enums/ERole.java
@@ -1,12 +1,5 @@
 package hdth.com.utils.enums;
 
 public enum ERole {
-
-    ROLE_ADMIN("Quản trị viên"), ROLE_USER("khách hàng"), ROLE_SHIPPER("Người giao hàng");
-
-    private String value;
-    ERole(String value){
-        this.value=value;
-    }
-
+    ROLE_ADMIN, ROLE_USER, ROLE_SHIPPER
 }
