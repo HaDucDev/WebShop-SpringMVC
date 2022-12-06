058adb2e27e7f1f6199c7e5fdec5b6b3158b818c
HaDucDev <haductrung392000@gmail.com>
2022-12-07 06:12:42 +0700

    update-model-1

---
+++ WebShopSPringMVC/src/main/java/hdth/com/model/ProducStatus.java
@@ -0,0 +1,31 @@
+package hdth.com.model;
+
+
+import hdth.com.utils.enums.ERole;
+import hdth.com.utils.enums.EStatusProduct;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table
+public class ProducStatus implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, unique = true)
+    private EStatusProduct name;
+
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "productstatus")
+    private Set<Product> products;
+}
---
+++ WebShopSPringMVC/src/main/java/hdth/com/utils/enums/EStatusProduct.java
@@ -0,0 +1,5 @@
+package hdth.com.utils.enums;
+
+public enum EStatusProduct {
+    STOCK,OUT_OF_STOCK
+}
