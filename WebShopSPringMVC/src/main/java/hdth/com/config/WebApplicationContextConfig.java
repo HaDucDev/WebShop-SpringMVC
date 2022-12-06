a232c7fffb30e44bc0ab263d5514313680d54de3
HaDucDev <haductrung392000@gmail.com>
2022-12-06 14:20:17 +0700

    java-config-demo-web

---
+++ WebShopSPringMVC/src/main/java/hdth/com/config/WebApplicationContextConfig.java
@@ -0,0 +1,32 @@
+package hdth.com.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "hdth.com.controller")
+public class WebApplicationContextConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public InternalResourceViewResolver geInternalResourceViewResolver(){
+        InternalResourceViewResolver resourceView= new InternalResourceViewResolver();
+        resourceView.setViewClass(JstlView.class);
+        resourceView.setPrefix("/WEB-INF/jsp/");
+        resourceView.setSuffix(".jsp");
+        return resourceView;
+    }
+
+}
