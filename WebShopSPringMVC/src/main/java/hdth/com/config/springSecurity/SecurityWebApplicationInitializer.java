def2a3a3fbfad9b8b4c3d17c1e667bcdeb397ecc
HaDucDev <haductrung392000@gmail.com>
2022-12-09 17:04:12 +0700

    khoi-tao-spring-security

---
+++ WebShopSPringMVC/src/main/java/hdth/com/config/springSecurity/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package hdth.com.config.springSecurity;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
